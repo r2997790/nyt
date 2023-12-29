d5b043f264bc4d38dea7b3749fb2e8506fe1199e
r2997790 <nayrsllew@gmail.com>
2023-12-29 19:15:40 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -9,22 +9,16 @@ window.addEventListener('load', checkOrientation);
 window.addEventListener('resize', checkOrientation);
 
 function checkOrientation() {
-    if (isMobileDevice()) {
-        if (window.innerWidth > window.innerHeight) {
-            // Landscape mode
-            document.getElementById('container').style.display = 'block';
-            document.querySelector('.rotate-message').style.display = 'none';
-        } else {
-            // Portrait mode
-            document.getElementById('container').style.display = 'none';
-            document.querySelector('.rotate-message').textContent = 'Please rotate your screen';
-            document.querySelector('.rotate-message').style.display = 'block';
-        }
+    if (window.innerWidth > window.innerHeight) {
+        // Landscape mode
+        document.getElementById('container').style.display = 'block';
+        document.querySelector('.rotate-message').style.display = 'none';
+    } else {
+        // Portrait mode
+        document.getElementById('container').style.display = 'none';
+        document.querySelector('.rotate-message').textContent = 'Please rotate your screen';
+        document.querySelector('.rotate-message').style.display = 'block';
     }
-}
-
-function isMobileDevice() {
-    return /Mobi|Android/i.test(navigator.userAgent);
 }
 
 async function toggleRecording() {
@@ -45,48 +39,4 @@ async function toggleRecording() {
     }
 }
 
-function handleDataAvailable(event) {
-    if (event.data && event.data.size > 0) {
-        recordedBlobs.push(event.data);
-    }
-}
-
-function startRecording(stream) {
-    recordedBlobs = [];
-    let options = { mimeType: 'video/webm;codecs=vp9' };
-    mediaRecorder = new MediaRecorder(stream, options);
-
-    mediaRecorder.ondataavailable = handleDataAvailable;
-    mediaRecorder.start();
-    startTimer();
-}
-
-function stopRecording() {
-    mediaRecorder.stop();
-    clearInterval(countdown);
-    document.getElementById('timer').textContent = '02:00';
-    const videoBlob = new Blob(recordedBlobs, { type: 'video/webm' });
-    const url = window.URL.createObjectURL(videoBlob);
-
-    const downloadLink = document.getElementById('downloadLink');
-    downloadLink.href = url;
-    downloadLink.download = 'recorded_video.webm';
-    downloadLink.style.display = 'block';
-}
-
-function startTimer() {
-    let time = 120; // 2 minutes in seconds
-    countdown = setInterval(() => {
-        let minutes = parseInt(time / 60, 10);
-        let seconds = parseInt(time % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        document.getElementById('timer').textContent = minutes + ":" + seconds;
-
-        if (--time < 0) {
-            toggleRecording();
-        }
-    }, 1000);
-}
+// ... Rest of the existing functions (handleDataAvailable, startRecording, stopRecording, startTimer)
